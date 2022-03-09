ca9c597cc1ce5253430150d32a4469eb9dd153a7
belleh71513 <belleh715313@gmail.com>
2022-03-09 23:39:07 +0800

    part2-3 keep going

--- static/js/index.js
+++ static/js/index.js
@@ -1,8 +1,13 @@
 
-let page = 0;
-let keyword = null;
 
-async function getAttImg() {
+
+
+
+async function getAttImg(page=0) {
+
+  const secondSection = document.querySelector(".secondSection")
+  const observerTarget = document.querySelector(".observerTarget")
+  // let keyword = null;
   // let url;
   // if(!keyword){
   //   url = `/api/attractions?page=${page}`
@@ -10,7 +15,7 @@ async function getAttImg() {
   // else{
   //   url = `/api/attractions?page=${page}&keyword=${keyword}`
   // };
-  const secondSection = document.querySelector(".secondSection")
+
 
   url = `/api/attractions?page=${page}`
 
@@ -18,10 +23,10 @@ async function getAttImg() {
   const data = await response.json();
 
   attData = data.data
+  nextPage = data.nextPage
+
 
   for(let attItem of attData){
-
-
     gridBox = document.createElement("div");
     gridBox.classList.add("gridBox")
 
@@ -51,8 +56,31 @@ async function getAttImg() {
     gridBox.append(boxImg, boxText);
     secondSection.appendChild(gridBox);
   }
+
+
+}
+const options = {
+  root: secondSection,
+  rootMargin: "0px 0px 200px 0px",
+  threshold:0
 }
 
-getAttImg()
+const callback = (entries, observer) => {
+  entries.forEach(entry => {
+    console.log(entry)
+    if(entry.isIntersecting){
+      if(nextPage){
+        getAttImg(nextPage);
+      }
+    }
+  });
+}
+let observer = new IntersectionObserver(callback, options)
+observer.observe(observerTarget)
+getAttImg();
+
+
+
+
 
 
