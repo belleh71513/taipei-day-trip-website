8598c04aca4ede9924449a8e04a24abf90e6013a
belleh71513 <belleh715313@gmail.com>
2022-03-17 03:19:26 +0800

    part2-5 finished

--- static/js/attraction.js
+++ static/js/attraction.js
@@ -1,4 +1,14 @@
 
+
+const imgContainer = document.querySelector(".img-container")
+const attractionName = document.querySelector(".attraction-name")
+const attractionCategoryMrt = document.querySelector(".attraction-category-mrt")
+const attracionDescription = document.querySelector(".attraction-description")
+const attractionAddressContent = document.querySelector(".attraction-address-content")
+const attractionTransportContent = document.querySelector(".attraction-transport-content")
+const slideIconWrap = document.querySelector(".slide-icon-wrap")
+
+
 let data = null;
 
 async function initData() {
@@ -12,27 +22,27 @@ async function initData() {
 
 function renderPage(){
   attData = data.data
-  const imgContainer = document.querySelector(".img-container")
-  const attractionName = document.querySelector(".attraction-name")
-  const attractionCategoryMrt = document.querySelector(".attraction-category-mrt")
-  const attracionDescription = document.querySelector(".attraction-description")
-  const attractionAddressContent = document.querySelector(".attraction-address-content")
-  const attractionTransportContent = document.querySelector(".attraction-transport-content")
-  const slideIconWrap = document.querySelector(".slide-icon-wrap")
-
-
-
 
   // 先將圖片render出來
-  attData["images"].forEach( imgURL => {
+  attData["images"].forEach( (imgURL, index) => {
     // 創造img element
     let img = document.createElement("img")
+    img.classList.add("slide-img")
     // 放進 img src
     img.src = imgURL
 
     // 創造圖片下方圓點
     let slideIcon = document.createElement("div")
     slideIcon.classList.add("slide-icon")
+    if(index !== 0){
+      img.classList.add("hidden")
+    }
+    if(index === 0){
+      slideIcon.classList.add("hidden")
+    }
+
+
+
 
     imgContainer.appendChild(img)
     slideIconWrap.appendChild(slideIcon)
@@ -43,7 +53,6 @@ function renderPage(){
   attracionDescription.textContent = attData["description"]
   attractionAddressContent.textContent = attData["address"]
   attractionTransportContent.textContent = attData["transport"]
-
 
 
 }
@@ -65,3 +74,35 @@ pmRadio.addEventListener("click", ()=>{
 amRadio.addEventListener("click", ()=>{
   bookingPrice.textContent = "新台幣 2000 元"
 })
+
+
+const arrowPre = document.querySelector(".arrow-pre");
+const arrowNext = document.querySelector(".arrow-next");
+let count = 0;
+
+
+arrowNext.addEventListener("click", ()=>{
+  const slideImg = document.querySelectorAll(".slide-img")
+  const slideIcon = document.querySelectorAll(".slide-icon");
+  count ++;
+  slideImg.forEach((img) =>{
+    img.classList.add("hidden")
+  })
+  slideIcon.forEach((icon) =>{
+    icon.classList.remove("hidden")
+  })
+
+  if(count > (slideImg.length-1)){
+    count = 0;
+  }
+  slideImg[count].classList.remove("hidden");
+  slideIcon[count].classList.add("hidden");
+  console.log(slideImg)
+
+})
+
+
+
+
+
+
