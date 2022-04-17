c2e2fc996afb8056c937d4ae7485774d5189ffe3
belleh71513 <belleh715313@gmail.com>
2022-04-17 18:49:34 +0800

    fix member api

--- api/api_member.py
+++ api/api_member.py
@@ -43,8 +43,6 @@ def api_member():
             }
           }
         member_list.append(data)
-      print(member_list)
-
       return jsonify({"data":member_list}), 200
     else:
       data = {
